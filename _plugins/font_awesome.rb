5a3ab1e884850a7c7ee4ec8a54d6ff5b2d3788d7
Jozef Balún <balunjozef@gmail.com>
2016-04-10 20:00:51 +0200

    Remove tem plugin fa

--- _plugins/font_awesome.rb
+++ _plugins/font_awesome.rb
@@ -1,6 +1,8 @@
 ##
 # The MIT License (MIT)
 #
+# !!!! PLUGIN SOM DOCASTNE ODSTRANIL ABY SOM NEMUSEL NAHRAVAT STALE GENEROVANU VERZIU !!!!
+#
 # Copyright (c) 2014 Ryan Morrissey
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -31,41 +33,41 @@
 #    {% icon fa-spinner fa-spin %}
 #    {% icon fa-shield fa-rotate-90 %}
 
-module Jekyll
-  class FontAwesomeTag < Liquid::Tag
+# module Jekyll
+#   class FontAwesomeTag < Liquid::Tag
 
-    def render(context)
-      if tag_contents = determine_arguments(@markup.strip)
-        icon_class, icon_extra = tag_contents[0], tag_contents[1]
-        icon_tag(icon_class, icon_extra)
-      else
-        raise ArgumentError.new <<-eos
-Syntax error in tag 'icon' while parsing the following markup:
+#     def render(context)
+#       if tag_contents = determine_arguments(@markup.strip)
+#         icon_class, icon_extra = tag_contents[0], tag_contents[1]
+#         icon_tag(icon_class, icon_extra)
+#       else
+#         raise ArgumentError.new <<-eos
+# Syntax error in tag 'icon' while parsing the following markup:
 
-  #{@markup}
+#   #{@markup}
 
-Valid syntax:
-  for icons: {% icon fa-camera-retro %}
-  for icons with size/spin/rotate: {% icon fa-camera-retro fa-lg %}
-eos
-      end
-    end
+# Valid syntax:
+#   for icons: {% icon fa-camera-retro %}
+#   for icons with size/spin/rotate: {% icon fa-camera-retro fa-lg %}
+# eos
+#       end
+#     end
 
-    private
+#     private
 
-    def determine_arguments(input)
-      matched = input.match(/\A(\S+) ?(\S+)?\Z/)
-      [matched[1].to_s.strip, matched[2].to_s.strip] if matched && matched.length >= 3
-    end
+#     def determine_arguments(input)
+#       matched = input.match(/\A(\S+) ?(\S+)?\Z/)
+#       [matched[1].to_s.strip, matched[2].to_s.strip] if matched && matched.length >= 3
+#     end
 
-    def icon_tag(icon_class, icon_extra = nil)
-      if icon_extra.empty?
-        "<i class=\"fa #{icon_class}\"></i>"
-      else
-        "<i class=\"fa #{icon_class} #{icon_extra}\"></i>"
-      end
-    end
-  end
-end
+#     def icon_tag(icon_class, icon_extra = nil)
+#       if icon_extra.empty?
+#         "<i class=\"fa #{icon_class}\"></i>"
+#       else
+#         "<i class=\"fa #{icon_class} #{icon_extra}\"></i>"
+#       end
+#     end
+#   end
+# end
 
-Liquid::Template.register_tag('icon', Jekyll::FontAwesomeTag)
+# Liquid::Template.register_tag('icon', Jekyll::FontAwesomeTag)
